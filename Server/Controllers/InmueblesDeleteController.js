553cfa2a08c88912f17611a6ce131158f89b1872
Santino <santiagorvrcn@gmail.com>
2023-12-28 10:19:56 -0500

    add endpoint delete

---
+++ Server/Controllers/InmueblesDeleteController.js
@@ -0,0 +1,38 @@
+const db = require('../Database/database.js')
+
+
+const deleteComercial = (req, res) => {
+    let comercialId = req.params.id
+    let query = `DELETE FROM comercial WHERE ID_Comercial = ?`
+
+
+    db.query(query, [comercialId], (err, result) => {
+        if(err){
+            console.log(`No se ha podido eliminar el comercial`, err);
+            res.status(500).json({ error: "Error al eliminar comercial"})
+            return
+        }
+        
+        res.status(200).send("Comercial eliminado exitosamente")
+    })
+}
+
+
+const deleteResidencia = (req, res) => {
+    let residenciaId = req.params.id
+    let query = `DELETE FROM residencial WHERE ID_Residencial = ?`
+
+    db.query(query, [residenciaId], (err, result) => {
+        if(err){
+            console.log(`No se ha podido eliminar la residencia`, err);
+            res.status(500).json({ error: "Error al eliminar residencia"})
+            return
+        }
+        
+        res.status(200).send("Residencia eliminada exitosamente")
+    })
+}
+
+
+
+module.exports = { deleteComercial, deleteResidencia }
--- Server/Routers/route.js
+++ Server/Routers/route.js
@@ -2,20 +2,39 @@ const express = require('express')
 const router = express.Router()
 const propiedadesInmuebles = require('../Controllers/InmueblesGetController.js')
 const guardarInmuebles = require('../Controllers/InmueblesPostController.js')
+const eliminarInmuebles = require('../Controllers/InmueblesDeleteController.js')
 // const auth = require('../Controllers/AuthController.js')
 
 
+
+//ENDPOINTS GET
 router.get('/residenciasFilter', propiedadesInmuebles.residencialFilter)
 router.get('/comercialFilter', propiedadesInmuebles.comercialFilter)
 router.get('/UserResidencia/:mail', propiedadesInmuebles.residenciaByMail)
 router.get('/UserComercial/:mail', propiedadesInmuebles.comercialByMail)
 router.get('/residenciaById/:id', propiedadesInmuebles.getResidenciaById)
 router.get('/comercialById/:id', propiedadesInmuebles.getComercialById)
+
+
+
+//ENDPOINTS POST
 router.post('/addResidencia', guardarInmuebles.addResidencia)
 router.post('/addComercial', guardarInmuebles.addComercial)
 router.post('/addUser', guardarInmuebles.addUser)
+
+
+
+//ENDPOINTS PUT
 router.put('/updateComercial/:id', guardarInmuebles.updateComercial)
 router.put('/updateResidencial/:id', guardarInmuebles.updateResidencial)
+
+
+//ENDPOINTS DELETE
+router.delete('/deleteComercial/:id', eliminarInmuebles.deleteComercial)
+router.delete('/deleteResidencial/:id', eliminarInmuebles.deleteResidencia)
+
+
+
 // router.put('/updateComercial/:id', guardarInmuebles.updateComercial)
 // router.post('/loginUser', auth.verifyUser)
 
