7394ded0b7d7379b7a768d5d6ff5574c2056792b
Santino <santiagorvrcn@gmail.com>
2024-01-18 10:28:16 -0500

    test 2

--- Server/Controllers/InmueblesGetController.js
+++ Server/Controllers/InmueblesGetController.js
@@ -249,7 +249,7 @@ const productos = (req, res, next) => {
     let datos = [
         {
             Image: 'https://i.imgur.com/QGgM211.png',
-            Text: `*Alejito \n Santino*`
+            Text: `En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mismo, los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los.`
         }
     ]
 
